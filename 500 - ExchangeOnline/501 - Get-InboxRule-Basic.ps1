aa79a4fe291a18053f590271ed390c27a956f510
informaticaeloy <eloyvilla@gmail.com>
2022-07-08 13:08:01 +0200

    Update 501 - Get-InboxRule-Basic.ps1

--- 500 - ExchangeOnline/501 - Get-InboxRule-Basic.ps1
+++ 500 - ExchangeOnline/501 - Get-InboxRule-Basic.ps1
@@ -1,1 +1,13 @@
  Get-InboxRule -Mailbox <mail del usuario>
+
+<#
+.OUTPUT
+Name                               Enabled Priority RuleIdentity
+----                               ------- -------- ------------
+Nombre Regla 1                     True    1        18321593459470859265
+Alarma Service                     True    2        18396221065213657201
+Alarma Bascula                     True    3        21816058815784652247
+[Warning] Replica                  True    4        59827666935214297665
+ALARMAS TEMPERATURAS               True    5        74239100521485253537
+
+#>
