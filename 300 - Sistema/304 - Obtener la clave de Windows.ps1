c60560946a215669f8d132d07f09957635756d34
informaticaeloy <eloyvilla@gmail.com>
2022-07-19 13:25:39 +0200

    Update 304 - Obtener la clave de Windows.ps1

--- 300 - Sistema/304 - Obtener la clave de Windows.ps1
+++ 300 - Sistema/304 - Obtener la clave de Windows.ps1
@@ -4,3 +4,6 @@ wmic path softwarelicensingservice get OA3xOriginalProductKey
 ## C:\Users\superuser>wmic path softwarelicensingservice get OA3xOriginalProductKey
 ## OA3xOriginalProductKey
 ## 3FJ2X-P8WHD-NY4DP-B98XC-FAKEQ
+
+## Si no sale clave, o directamente sale esta: VK7JG-NPHTM-C97JM-9MPGT-3V66T, o no está licenciado o no es original
+## Se puede usar este software para confirmar: https://www.nirsoft.net/utils/product_cd_key_viewer.html
