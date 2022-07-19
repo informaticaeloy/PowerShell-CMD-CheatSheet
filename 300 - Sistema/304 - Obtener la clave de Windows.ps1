fdb7de6a48c769e0757ca551d8ba3fee291998fa
informaticaeloy <eloyvilla@gmail.com>
2022-07-19 11:48:05 +0200

    Update 304 - Obtener la clave de Windows.ps1

--- 300 - Sistema/304 - Obtener la clave de Windows.ps1
+++ 300 - Sistema/304 - Obtener la clave de Windows.ps1
@@ -1,1 +1,6 @@
 wmic path softwarelicensingservice get OA3xOriginalProductKey
+
+## OUTPUT
+## C:\Users\superuser>wmic path softwarelicensingservice get OA3xOriginalProductKey
+## OA3xOriginalProductKey
+## 3FJ2X-P8WHD-NY4DP-B98XC-FAKEQ
