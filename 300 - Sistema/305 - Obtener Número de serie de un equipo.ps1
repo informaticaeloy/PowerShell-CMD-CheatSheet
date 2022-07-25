db9c6720adcb36f7f283f08c8de8dc4d52428a8e
informaticaeloy <eloyvilla@gmail.com>
2022-07-25 10:35:33 +0200

    Update 305 - Obtener Número de serie de un equipo.ps1

--- 300 - Sistema/305 - Obtener Número de serie de un equipo.ps1
+++ 300 - Sistema/305 - Obtener Número de serie de un equipo.ps1
@@ -1,8 +1,5 @@
 ### DEL EQUIPO LOCAL
-
 Get-CimInstance -ClassName Win32_BIOS -Property SerialNumber | Select-Object -ExpandProperty SerialNumber
 
-
 ### DE UN EQUIPO REMOTO
-
 Get-CimInstance -ComputerName "nombre-del-host" -ClassName Win32_BIOS -Property SerialNumber | Select-Object -ExpandProperty SerialNumber
