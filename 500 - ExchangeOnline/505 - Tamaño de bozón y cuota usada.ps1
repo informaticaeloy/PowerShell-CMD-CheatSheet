649bb084e0802ec271be6532e77291fa3dcca47f
informaticaeloy <eloyvilla@gmail.com>
2022-08-03 12:58:49 +0200

    Update 505 - Tamaño de bozón y cuota usada.ps1

--- 500 - ExchangeOnline/505 - Tamaño de bozón y cuota usada.ps1
+++ 500 - ExchangeOnline/505 - Tamaño de bozón y cuota usada.ps1
@@ -1,4 +1,5 @@
 
-$array = Get-EXOMailBox eloyvilla@jorgesl.com
+$array = Get-EXOMailBox <mail_del_usuario>
+
 Get-ExoMailboxStatistics -Identity $array.UserPrincipalname -PropertySets All | select DisplayName,ItemCount,TotalItemSize, SystemMessageSizeShutoffQuota, $array.UserPrincipalName
 
