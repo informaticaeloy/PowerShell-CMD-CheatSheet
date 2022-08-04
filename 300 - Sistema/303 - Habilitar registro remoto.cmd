d575b181210de2f7ab42c9dfbde2ca13dda03996
informaticaeloy <eloyvilla@gmail.com>
2022-08-04 13:39:07 +0200

    Update 303 - Habilitar registro remoto.cmd

--- 300 - Sistema/303 - Habilitar registro remoto.cmd
+++ 300 - Sistema/303 - Habilitar registro remoto.cmd
@@ -13,26 +13,26 @@ C:\Users\usuario>sc \\PC-INF019 config remoteregistry start= auto
 sc \\<NombreDeEquipo_o_IP> start remoteregistry
 
 # Devuelve algo similar a esto con el resultado:
-NOMBRE_SERVICIO: remoteregistry
-        TIPO               : 30  WIN32
-        ESTADO             : 3  STOP_PENDING
-                                (STOPPABLE, NOT_PAUSABLE, IGNORES_SHUTDOWN)
-        CÓD_SALIDA_WIN32   : 1066  (0x42a)
-        CÓD_SALIDA_SERVICIO: 0  (0x0)
-        PUNTO_COMPROB.     : 0x2
-        INDICACIÓN_INICIO  : 0xbb8
+# NOMBRE_SERVICIO: remoteregistry
+#         TIPO               : 30  WIN32
+#         ESTADO             : 3  STOP_PENDING
+#                                 (STOPPABLE, NOT_PAUSABLE, IGNORES_SHUTDOWN)
+#         CÓD_SALIDA_WIN32   : 1066  (0x42a)
+#         CÓD_SALIDA_SERVICIO: 0  (0x0)
+#         PUNTO_COMPROB.     : 0x2
+#         INDICACIÓN_INICIO  : 0xbb8
         
 # Para el servicio
 sc \\<NombreDeEquipo_o_IP> stop remoteregistry
 
 # Devuelve algo similar a esto con el resultado:
-NOMBRE_SERVICIO: remoteregistry
-        TIPO               : 30  WIN32
-        ESTADO             : 2  START_PENDING
-                                (NOT_STOPPABLE, NOT_PAUSABLE, IGNORES_SHUTDOWN)
-        CÓD_SALIDA_WIN32   : 0  (0x0)
-        CÓD_SALIDA_SERVICIO: 0  (0x0)
-        PUNTO_COMPROB.     : 0x1
-        INDICACIÓN_INICIO  : 0xbb8
-        PID                : 11584
-        MARCAS         :
+# NOMBRE_SERVICIO: remoteregistry
+#         TIPO               : 30  WIN32
+#         ESTADO             : 2  START_PENDING
+#                                 (NOT_STOPPABLE, NOT_PAUSABLE, IGNORES_SHUTDOWN)
+#         CÓD_SALIDA_WIN32   : 0  (0x0)
+#         CÓD_SALIDA_SERVICIO: 0  (0x0)
+#         PUNTO_COMPROB.     : 0x1
+#         INDICACIÓN_INICIO  : 0xbb8
+#         PID                : 11584
+#         MARCAS         :
