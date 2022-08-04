c4f17ebc31c624f4042c8b1258a55e88b5ab99b3
informaticaeloy <eloyvilla@gmail.com>
2022-08-04 12:39:32 +0200

    Create 509 - Tamaño carpeta archivados y cuota(todos usuario).ps1

---
+++ 500 - ExchangeOnline/509 - Tamaño carpeta archivados y cuota(todos usuario).ps1
@@ -0,0 +1,9 @@
+PS C:\Windows\system32> get-mailbox | Get-MailboxFolderStatistics | Where-Object {$_.name -like "Archivos"} | select identity, Foldersize, storagequota
+
+## Salida:
+## Identity              FolderSize    StorageQuota
+## --------              ----------    ------------
+## John Doe\Archivos     0 B (0 bytes) 48 GB (51,539,607,552 bytes)
+## Maikel Nai\Archivos   0 B (0 bytes) 48 GB (51,539,607,552 bytes)
+## Llanero solitario\... 0 B (0 bytes) 48 GB (51,539,607,552 bytes)
+
