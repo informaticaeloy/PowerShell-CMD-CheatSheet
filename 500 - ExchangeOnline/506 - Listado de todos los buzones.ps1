bb306f4cc5bc231881b1a9746df9c2837b2c5929
informaticaeloy <eloyvilla@gmail.com>
2022-08-04 12:21:37 +0200

    Create 506 - Listado de todos los buzones.ps1

---
+++ 500 - ExchangeOnline/506 - Listado de todos los buzones.ps1
@@ -0,0 +1,10 @@
+Get-Mailbox -ResultSize Unlimited
+
+## Nos da un resultado similar a este:
+
+## Name                      Alias           Database                       ProhibitSendQuota    ExternalDirectoryObjectId
+## ----                      -----           --------                       -----------------    -------------------------
+## Angel Contoso             angelcontoso    EURPR06DG256-db198             49.5 GB (53,150,2... a26fakef-8665-49c8-bcd4-52585a13adbf
+## John Doe                  johndoe         EURPR06DG321-db051             49.5 GB (53,150,2... 9fakebbc-0cf0-439f-924c-6affake1d266
+## Maikel Nai                maikelnai       EURPR06DG420-db058             49.5 GB (53,150,2... b6afake4-d22c-4074-ba57-1bfake15599b
+## Willy Illy                willyilly       EURPR06DG295-db213             1.979 GB (2,125,4... b14a936a-4bce-4f0b-830b-230d455fake0
