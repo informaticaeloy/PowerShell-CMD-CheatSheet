9f05b7a110724bb6072314965f243954c0af73bf
informaticaeloy <eloyvilla@gmail.com>
2022-08-12 09:18:39 +0200

    Create 306 - Añadir opciones energía Windows 10.ps1

---
+++ 300 - Sistema/306 - Añadir opciones energía Windows 10.ps1
@@ -0,0 +1,10 @@
+## Ejecutar estos comandos como administrador en el propio equipo o mediante una sesión remota de PS para añadir estos perfiles si no aparecen
+
+## Modo Economizador
+powercfg -duplicatescheme a1841308-3541-4fab-bc81-f71556f20b4a
+
+## Modo Equilibrado
+powercfg -duplicatescheme 381b4222-f694-41f0-9685-ff5bb260df2e
+
+## Modo Alto rendimiento
+powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
