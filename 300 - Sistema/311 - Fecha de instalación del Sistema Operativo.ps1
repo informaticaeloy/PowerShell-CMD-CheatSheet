d5773c84d9bd3dc3691036e4a7af091ded121a55
informaticaeloy <eloyvilla@gmail.com>
2022-09-07 17:18:26 +0200

    Update 311 - Fecha de instalación del Sistema Operativo.ps1

--- 300 - Sistema/311 - Fecha de instalación del Sistema Operativo.ps1
+++ 300 - Sistema/311 - Fecha de instalación del Sistema Operativo.ps1
@@ -1,7 +1,7 @@
 # en local
 (Get-CimInstance -Class Win32_OperatingSystem).InstallDate
 # en remoto
-PS C:\Windows\system32> (Get-CimInstance -Class Win32_OperatingSystem -ComputerName "NOMBRE_EQUIPO").InstallDate
+(Get-CimInstance -Class Win32_OperatingSystem -ComputerName "NOMBRE_EQUIPO").InstallDate
 
 # Ejemplo
 PS C:\Windows\system32> (Get-CimInstance -Class Win32_OperatingSystem).InstallDate
