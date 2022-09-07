46476b4f5b916fec7421c6b8976a7c7599fd6251
informaticaeloy <eloyvilla@gmail.com>
2022-09-07 12:28:47 +0200

    Update 303 - Habilitar registro remoto.cmd

--- 300 - Sistema/303 - Habilitar registro remoto.cmd
+++ 300 - Sistema/303 - Habilitar registro remoto.cmd
@@ -1,4 +1,4 @@
-# SERVICIO "ESCRITORIO REMOTO"
+# SERVICIO "REGISTRO REMOTO"
 # Este servicio permite conectarnos a equipos remotos para cambiar, eliminar o agregar claves y valores de registro,
 # el cual nos permite conectarnos directamente a las estructuras de registro en otro ordenador usando regedit.
 
