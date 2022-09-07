afb42b82533eb53cbc81a3f9d9a5b2e55d08239a
informaticaeloy <eloyvilla@gmail.com>
2022-09-07 16:49:01 +0200

    Update 308 - Obtener inventario Hardware CPU de un equipo.ps1

--- 300 - Sistema/308 - Obtener inventario Hardware CPU de un equipo.ps1
+++ 300 - Sistema/308 - Obtener inventario Hardware CPU de un equipo.ps1
@@ -1,1 +1,16 @@
--ComputerName
+# Información sobre la CPU instalada
+
+# TODA LA INFORMACIÓN DE LA CPU #######################################################################################################
+###########################################################################################################################################
+## en local
+Get-CimInstance -ClassName Win32_Processor
+## en remoto
+Get-CimInstance -ClassName Win32_Processor -ComputerName "NOMBRE_EQUIPO"
+
+# Ejemplo
+PS C:\Windows\system32> Get-CimInstance -ClassName Win32_Processor
+
+# OUTPUT
+DeviceID Name                                     Caption                               MaxClockSpeed SocketDesignation Manufacturer
+-------- ----                                     -------                               ------------- ----------------- ------------
+CPU0     Intel(R) Core(TM) i5-10400 CPU @ 2.90GHz Intel64 Family 6 Model 165 Stepping 3 2904          U3E1              GenuineIntel
