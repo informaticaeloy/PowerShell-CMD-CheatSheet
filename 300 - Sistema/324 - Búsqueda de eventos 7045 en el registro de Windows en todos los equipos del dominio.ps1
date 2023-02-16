4f4f056761f8dcc7c509098da52fe35a4d11857f
informaticaeloy <eloyvilla@gmail.com>
2023-02-16 12:16:45 +0100

    Update 324 - Búsqueda de eventos 7045 en el registro de Windows en todos los equipos del dominio.ps1

--- 300 - Sistema/324 - Búsqueda de eventos 7045 en el registro de Windows en todos los equipos del dominio.ps1
+++ 300 - Sistema/324 - Búsqueda de eventos 7045 en el registro de Windows en todos los equipos del dominio.ps1
@@ -1,15 +1,18 @@
 clear 
 $Tab = [char]9
+$evento_encontrado    = 0
+$evento_no_encontrado = 0
+$equipo_apagado       = 0
 
-$MI_OU = 'OU=INFORMATICA,DC=dominio,DC=loc'
+$MIOU = 'OU=INFORMATICA,DC=dominio,DC=loc'
 
-Write-Host "Calculando el número de equipos en $MI_OU ...."
+Write-Host "Calculando el número de equipos en $MIOU ...."
 
-$EQUIPOS_OU  = Get-ADComputer -SearchBase $MI_OU -Filter * -Properties * | Select-Object -ExpandProperty Name
+$EQUIPOS_UO  = Get-ADComputer -SearchBase $MIOU -Filter * -Properties * | Select-Object -ExpandProperty Name
 
 $num_equipos = 0
 
-foreach ($CADA_EQUIPO in $EQUIPOS_OU)
+foreach ($CADA_EQUIPO in $EQUIPOS_UO)
 {
     $num_equipos += 1
 }
@@ -24,9 +27,9 @@ $XPath = "*[ (EventData[Data[@Name='ServiceName']='uvnc_service'] or EventData[D
 
 foreach ($CADA_EQUIPO in $EQUIPOS_UO)
 { 
-    $i      += 1
+    $i     += 1
     $result = $null
-        
+    
     if (Test-Connection $CADA_EQUIPO -Quiet)
     {
         Write-Host "✔ (equipo $i de $num_equipos) -> Ejectuando en : $CADA_EQUIPO"  -ForegroundColor White -BackgroundColor Green
@@ -36,22 +39,26 @@ foreach ($CADA_EQUIPO in $EQUIPOS_UO)
         }
         catch
         {
-            
+           ## Aquí podríamos poner alguna instrucción a ejecutar cuando de error el Get-WinEvent 
         }
         if ($null -eq $result)
         {
             Write-Host "$tab ::Evento NO ENCONTRADO" -ForegroundColor White -BackgroundColor DarkGray
+            $evento_no_encontrado += 1
         }
         else
         {
             Write-Host "$tab ::Evento ENCONTRADO. Lo añado al fichero" -ForegroundColor White -BackgroundColor Blue
             $result | Out-File -FilePath C:\temp\EVENTOS_7045.txt -Append
+            $evento_encontrado += 1
         }
     }
     else
     {
         Write-Host "❌ (equipo $i de $num_equipos) -> Ejectuando en : $CADA_EQUIPO -> Parece que está apagado"  -ForegroundColor White -BackgroundColor Red
+        $equipo_apagado += 1
     }
 }
 
-Write-Host "Todo terminado !!!"
+Write-Host "Todo terminado !!! "
+Write-Host "$evento_encontrado equipos AFECTADOS , $evento_no_encontrado equipos NO AFECTADOS y $equipo_apagado equipos apagados"
