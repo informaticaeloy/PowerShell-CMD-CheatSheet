3fc6c3c9e14533504bbe480ffd40758666011167
informaticaeloy <eloyvilla@gmail.com>
2024-01-04 12:46:26 +0100

    Create 803 - Listar usuarios con check "password never expires".ps1

---
+++ 800 - Active Directory/803 - Listar usuarios con check "password never expires".ps1
@@ -0,0 +1,8 @@
+# Listado de usuarios del active directory con el check de "la contraseña nunca expira"
+Get-ADUser -Filter * -Properties * | select name, passwordneverexpires | export-csv -path c:\temp\password_nunca_expira.csv
+
+# Lo mismo pero exportando la salida a un CSV
+Get-ADUser -Filter * -Properties * | select name, passwordneverexpires | export-csv -path c:\temp\password_nunca_expira.csv
+
+# Lo mismo pero con salida en una vista de cuadrícula
+Get-ADUser -Filter * -Properties * | select name, passwordneverexpires | Out-GridView
