c2c56ee0767e3ac5442c83ba8f1d99376819e5a0
informaticaeloy <eloyvilla@gmail.com>
2025-08-22 08:26:29 +0200

    Update 515 - Listar usuario al que pertenece un ID de buzón.ps1

--- 500 - ExchangeOnline/515 - Listar usuario al que pertenece un ID de buzón.ps1
+++ 500 - ExchangeOnline/515 - Listar usuario al que pertenece un ID de buzón.ps1
@@ -1,2 +1,5 @@
 Connect-ExchangeOnline -UserPrincipalName tu_mail_con_permisos@contoso.com
 Get-Mailbox -Identity <ID_de_buzón_ae622c05-077f-fake-a5cd-93f2fakefb61>
+
+*** Para ver más claro datos relevantes como el DisplayName o el primarySmtpAddress:
+Get-Mailbox -Identity <ID_de_buzón_ae622c05-077f-fake-a5cd-93f2fakefb61> | Format-List DisplayName,PrimarySmtpAddress
