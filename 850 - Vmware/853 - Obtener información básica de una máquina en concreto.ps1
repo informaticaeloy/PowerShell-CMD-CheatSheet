0ed8ed327e6aaaf8bb4d9becd2e55d703a1a4aac
informaticaeloy <eloyvilla@gmail.com>
2025-09-23 12:13:29 +0200

    Create 853 - Obtener información básica de una máquina en concreto.ps1

---
+++ 850 - Vmware/853 - Obtener información básica de una máquina en concreto.ps1
@@ -0,0 +1,9 @@
+# Información básica
+get-vm <nombre_máquina>
+
+# Ejemplo de salida
+PS C:\Users\espinete> get-vm SKYNET
+
+Name                 PowerState Num CPUs MemoryGB
+----                 ---------- -------- --------
+SKYNET               PoweredOn  4096     2955,00
