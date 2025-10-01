316df6e7791aa29b10f23822e8e1cb1c3312496b
informaticaeloy <eloyvilla@gmail.com>
2025-10-01 11:52:22 +0200

    Update 334 - Gestión contraseña administrador local LAPS.ps1

--- 300 - Sistema/334 - Gestión contraseña administrador local LAPS.ps1
+++ 300 - Sistema/334 - Gestión contraseña administrador local LAPS.ps1
@@ -10,3 +10,15 @@ Get-LAPSADPassword -Identity COMPUTER -AsPlainText
 
 # (Mostrar contraseña actual e historial de contrasañas del administrador local en un equipo)
 Get-LAPSADPassword COMPUTER -IncludeHistory -AsPlainText 
+
+# Reset-LAPSPassword (Resetear password, si se tiene permisos)
+Invoke-Command -ComputerName COMPUTER -ScriptBlock {Reset-LAPSPassword}
+
+# Resetear contraseña en equipo remoto. (Establece la hora de expiración como la hora actual. En la siguiente evaluación de la polítca, rota la contraseña)
+Set-LapsADPasswordExpirationTime -Identity COMPUTER 
+
+# Permisos sobre una OU
+Find-LapsADExtendedRights -Identity "OU=.." | fl 
+
+# Fuerza la aplicación de la política sin tener que espera a que cumpla el tiempo de 1h
+Invoke-LapsPolicyProcessing 
