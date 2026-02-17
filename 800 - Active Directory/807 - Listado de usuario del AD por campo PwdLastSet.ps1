398fe03f2ecd791ec4b800bef47e46bfca2a1b65
informaticaeloy <eloyvilla@gmail.com>
2026-02-17 13:54:42 +0100

    Add distinguishedName to AD user export

--- 800 - Active Directory/807 - Listado de usuario del AD por campo PwdLastSet.ps1
+++ 800 - Active Directory/807 - Listado de usuario del AD por campo PwdLastSet.ps1
@@ -1,3 +1,8 @@
 Get-ADUser -Filter "*" -Properties PwdLastSet | Sort-Object Name | 
     Select-Object Name, @{Name='PwdLastSet';Expression={[DateTime]::FromFileTime($_.PwdLastSet)}} | 
     Export-Csv -Path "c:\temp\test.csv" -NoTypeInformation
+
+# Añadiendo el distinguisedName
+Get-ADUser -Filter "*" -Properties PwdLastSet, distinguishedName | Sort-Object Name | 
+    Select-Object Name, @{Name='PwdLastSet';Expression={[DateTime]::FromFileTime($_.PwdLastSet)}}, distinguishedName | 
+    Export-Csv -Path "c:\temp\test.csv" -NoTypeInformation
