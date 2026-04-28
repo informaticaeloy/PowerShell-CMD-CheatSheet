e639b160d235c1b51e98c0345e1b97cf7bcfa37f
informaticaeloy <eloyvilla@gmail.com>
2026-04-28 11:22:48 +0200

    Create 520 - Listado de reglas de los usuarios de MS Exchange que reenvían correos.ps1

---
+++ 500 - ExchangeOnline/520 - Listado de reglas de los usuarios de MS Exchange que reenvían correos.ps1
@@ -0,0 +1,11 @@
+Connect-ExchangeOnline
+
+Get-EXOMailbox -ResultSize Unlimited | ForEach-Object {
+    Get-InboxRule -Mailbox $_.UserPrincipalName -ErrorAction SilentlyContinue |
+    Where-Object { $_.ForwardTo -ne $null -or $_.RedirectTo -ne $null } |
+    Select-Object @{Name="Mailbox"; Expression={$_.MailboxOwnerId}},
+                  Name,
+                  ForwardTo,
+                  RedirectTo,
+                  Enabled
+} | Export-Csv "C:\Reports\ReglasReenvioExterno.csv" -NoTypeInformation -Encoding UTF8
