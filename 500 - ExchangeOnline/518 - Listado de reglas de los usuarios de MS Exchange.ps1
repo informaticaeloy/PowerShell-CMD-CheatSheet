e3954fbad742444b9559eb7ff8a34cacf1711ae6
informaticaeloy <eloyvilla@gmail.com>
2026-04-28 11:21:58 +0200

    Enhance CSV export with additional InboxRule fields
    Updated the script to include Priority and Description fields in the exported CSV.

--- 500 - ExchangeOnline/518 - Listado de reglas de los usuarios de MS Exchange.ps1
+++ 500 - ExchangeOnline/518 - Listado de reglas de los usuarios de MS Exchange.ps1
@@ -1,11 +1,10 @@
 Connect-ExchangeOnline
 
 Get-EXOMailbox -ResultSize Unlimited | ForEach-Object {
-    Get-InboxRule -Mailbox $_.UserPrincipalName -ErrorAction SilentlyContinue |
-    Where-Object { $_.ForwardTo -ne $null -or $_.RedirectTo -ne $null } |
+    Get-InboxRule -Mailbox $_.UserPrincipalName -ErrorAction SilentlyContinue | 
     Select-Object @{Name="Mailbox"; Expression={$_.MailboxOwnerId}},
                   Name,
-                  ForwardTo,
-                  RedirectTo,
-                  Enabled
-} | Export-Csv "C:\Temp\ReglasReenvioExterno.csv" -NoTypeInformation -Encoding UTF8
+                  Enabled,
+                  Priority,
+                  Description
+} | Export-Csv "C:\Reports\TodasLasReglas.csv" -NoTypeInformation -Encoding UTF8
