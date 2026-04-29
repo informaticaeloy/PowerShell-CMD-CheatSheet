17a8eb0d21d4ca0b8fb56b46357524af6ee75c4b
informaticaeloy <eloyvilla@gmail.com>
2026-04-29 12:34:18 +0200

    Update 519 - Listado de reglas de los usuarios de MS Exchange que eliminan correos.ps1

--- 500 - ExchangeOnline/519 - Listado de reglas de los usuarios de MS Exchange que eliminan correos.ps1
+++ 500 - ExchangeOnline/519 - Listado de reglas de los usuarios de MS Exchange que eliminan correos.ps1
@@ -8,3 +8,21 @@ Get-EXOMailbox -ResultSize Unlimited | ForEach-Object {
                   DeleteMessage,
                   Description
 } | Export-Csv "C:\Reports\ReglasEliminacion.csv" -NoTypeInformation -Encoding UTF8
+
+
+
+
+#### VERSION MEJORADA
+Get-EXOMailbox -ResultSize Unlimited | ForEach-Object {
+    $mailbox = $_
+    
+    Get-InboxRule -Mailbox $mailbox.UserPrincipalName -ErrorAction SilentlyContinue |
+    Where-Object { $_.DeleteMessage -eq $true } |
+    Select-Object @{Name="MailboxUPN"; Expression={$mailbox.UserPrincipalName}},
+                  @{Name="MailboxDisplayName"; Expression={$mailbox.DisplayName}},
+                  @{Name="MailboxAlias"; Expression={$mailbox.Alias}},
+                  Name,
+                  DeleteMessage,
+                  Description,
+                  StopProcessingRules
+} | Export-Csv "C:\temp\ReglasEliminacion.csv" -NoTypeInformation -Encoding UTF8
